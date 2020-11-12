c8cf90837b92295833882045fc23de1e1c11839a
Lucas Moncada <lucas.moncada08@gmail.com>
2020-11-11 21:16:28 -0500

    Added Basic Calendar View

--- angular-lab4/src/app/app.component.ts
+++ angular-lab4/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { EventSettingsModel, WorkWeekService} from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.css'],
+  providers: [WorkWeekService],
 })
 export class AppComponent {
   title = 'Western Timetable';
+  public selectedDate: Date = new Date(2021, 8, 6);
+  // public eventSettings: EventSettingsModel = { dataSource: scheduleData };
+
+  constructor() { }
+
+  public keyConfigs = {
+    select: "space",
+    home: "ctrl+home",
+    end: "ctrl+end"
+  }
+
 }
--- angular-lab4/src/app/app.module.ts
+++ angular-lab4/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
+import { DayService, WeekService, WorkWeekService, MonthService, AgendaService, MonthAgendaService } from '@syncfusion/ej2-angular-schedule';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScheduleModule
   ],
-  providers: [],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService, MonthAgendaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
